30283a1b68723ef96d0b031f9bfd73b0ba30a5ed
Tim McClory <tjmcclory@gmail.com>
2013-10-16 19:25:30 -0400

    Set up angular

---
+++ www/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var wwwApp = angular.module('wwwApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
